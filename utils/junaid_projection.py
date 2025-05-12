ba96346680749ced740d38730f1c941f52f4f251
Junaid Iqbal Khan <dianujkotov15@gmail.com>
2025-05-12 14:44:56 +0500

    Update junaid_projection.py

--- utils/junaid_projection.py
+++ utils/junaid_projection.py
@@ -586,3 +586,195 @@ def concrete_pathetic_projection(
     return (depth_img, reflectance_img, proj_x, proj_y, beeth_points, points_2_image_indices)
   
 
+
+
+def concrete_pathetic_projection_extended(
+    points, 
+    reflectivity, 
+    labels,
+    num_classes,
+    W, 
+    H,
+    fov_up = 2.0,
+    fov_down = -24.9,
+    normalize = False,
+    visualize = True,
+    inverted_depth = True,
+):
+
+    def indices_from_elevation(points, beam_alt_deg):
+        beam_alt = np.deg2rad(beam_alt_deg)           
+        depth    = np.linalg.norm(points, axis=1) + 1e-6
+        elev     = np.arcsin(points[:, 2] / depth)                
+        return np.argmin(np.abs(elev[:, None] - beam_alt[None, :]), axis=1)
+    
+
+
+    r = np.linalg.norm(points, axis=1) + 1e-6
+    elevation = np.arcsin(points[:, 2] / r)  # [-π/2, +π/2] 
+    elevation_nrom = (elevation + abs(fov_down)) / (abs(fov_up) + abs(fov_down))  # [0, 1]
+    order = np.argsort(elevation_nrom)[::-1]  # top to bottom
+    points = points[order]
+    reflectivity = reflectivity[order]       
+    labels = labels[order] 
+    # num_p_bin = np.ceil(len(points)/H).astype(int)
+    # bin_indices = np.array([[i] * (num_p_bin) for i in range(H)]).flatten()
+    # bin_indices = bin_indices[:len(points)]
+
+    beam_alts = np.linspace(fov_down, fov_up, H)         
+    bin_indices = indices_from_elevation(points, beam_alts) 
+    # bin_indices = np.flip(bin_indices, axis=0)
+    
+
+    cum_points =[]
+    cum_depth_map = []
+    cum_reflectance_map = []
+    cum_reflectivity = []
+    cum_depth = []
+    proj_x = []
+    proj_y = []
+
+    ordered_bin_indices = np.sort(np.unique(bin_indices))
+    ordered_bin_indices = np.flip(ordered_bin_indices, axis=0)  # top to bottom
+
+
+    point_indices = []
+    orcale_indices = np.arange(len(points))
+
+    beeth_points = points.copy()
+    beeth_labels = labels.copy()
+
+
+    # within each bin, acendingly sort the points by azimuth, and fill the rows of image form left to right
+    for i in ordered_bin_indices:
+        bin_mask = (bin_indices == i)
+        # print(f"bin {i} has {np.sum(bin_mask)} points")
+        bin_points = points[bin_mask]
+        bin_reflectivity = reflectivity[bin_mask]
+        curr_indices = orcale_indices[bin_mask]
+
+
+        bin_azimuth = -np.arctan2(bin_points[:, 1], - bin_points[:, 0])
+        bin_azimuth = (bin_azimuth + np.pi) /(2 * np.pi)  # Normalize to [0, 1]
+
+        order_sort = np.argsort(bin_azimuth)
+        sorted_bin_azimuth = bin_azimuth[order_sort]
+        sorted_bin_points = bin_points[order_sort]
+        sorted_bin_reflectivity = bin_reflectivity[order_sort]
+        sorted_curr_indices = curr_indices[order_sort]
+        sorted_bin_depth = np.linalg.norm(sorted_bin_points, 2, axis=1) + 1e-6
+
+        bin_azimuth_indices = (sorted_bin_azimuth* (W-1)).astype(np.int32)
+        depth_pixels = np.zeros(W).astype(np.float32)
+        refl_pixels = np.zeros(W).astype(np.float32)
+
+        if not inverted_depth:
+            depth_pixels[bin_azimuth_indices] = sorted_bin_depth
+        else:
+            depth_pixels[bin_azimuth_indices] = 1.0/(0.5+sorted_bin_depth)
+            
+        refl_pixels[bin_azimuth_indices] = sorted_bin_reflectivity
+
+        cum_depth_map.append(depth_pixels)
+        cum_reflectance_map.append(refl_pixels)
+
+        proj_x.append(bin_azimuth_indices)
+        proj_y.append(np.ones_like(bin_azimuth_indices) * i)
+        cum_points.append(sorted_bin_points)
+        cum_reflectivity.append(sorted_bin_reflectivity)
+        cum_depth.append(sorted_bin_depth)
+        point_indices.append(sorted_curr_indices)
+
+    depth_img = np.vstack(cum_depth_map)  # shape (H, W)
+    reflectance_img = np.vstack(cum_reflectance_map) # shape (H, W)
+    proj_x = np.concatenate(proj_x)
+    proj_y = np.concatenate(proj_y)
+    cum_points = np.concatenate(cum_points)
+    cum_reflectivity = np.concatenate(cum_reflectivity)
+    cum_depth = np.concatenate(cum_depth)
+    cum_point_indices = np.concatenate(point_indices)
+    ref_proj_x = proj_x.copy()
+    ref_proj_y = proj_y.copy()
+
+    sort_order = np.argsort(cum_point_indices)
+    cum_point_indices = cum_point_indices[sort_order]
+    ref_proj_x = ref_proj_x[sort_order]
+    ref_proj_y = ref_proj_y[sort_order]
+    beeth_points = beeth_points[sort_order]
+    beeth_labels = beeth_labels[sort_order]
+
+    print(f"Size of Point Indices: {cum_point_indices.shape}")
+    print(f"Size of Points: {beeth_points.shape}")
+    print(f"Size of Labels: {beeth_labels.shape}")
+
+    points_2_image_indices = {}
+
+    for i in range(len(cum_point_indices)):
+        points_2_image_indices[cum_point_indices[i]] = [ref_proj_y[i], ref_proj_x[i]]
+
+    cl_maps = []
+    unique_clsses = np.unique(beeth_labels)
+    print(f"Unique classes in reference labels: {unique_clsses}")
+    for cl in range(len(unique_clsses)):
+        mask = (beeth_labels == unique_clsses[cl])
+        mask = np.squeeze(mask, axis = 1)
+        idx = cum_point_indices[mask]
+
+        cl_map = np.zeros((H, W, 3), dtype=np.float32)
+        
+        coord_x = []
+        coord_y = []
+        for i in range(len(idx)):
+            coord_x.append(points_2_image_indices[idx[i]][0])
+            coord_y.append(points_2_image_indices[idx[i]][1])
+
+        coord_x = np.array(coord_x)
+        coord_y = np.array(coord_y)
+
+
+        # Generate a single random color for the current class
+        random_color = np.random.rand(3)
+        cl_map[coord_x, coord_y, 0] = random_color[0]
+        cl_map[coord_x, coord_y, 1] = random_color[1]
+        cl_map[coord_x, coord_y, 2] = random_color[2]
+
+
+        cl_maps.append(cl_map)
+
+
+    # normalize depth and reflectance values to [0, 1]
+    if normalize:
+        depth_img = (depth_img - depth_img.min()) / (depth_img.max() - depth_img.min())
+        reflectance_img = (reflectance_img - reflectance_img.min()) / (reflectance_img.max() - reflectance_img.min())
+
+
+
+    if visualize:
+        plt.figure(figsize=(12, 4))
+        plt.subplot(2, 1, 1)
+        plt.title("V2 Depth (m)")
+        plt.imshow(depth_img, cmap='gray',
+                vmin=0, vmax=np.percentile(depth_img, 99))
+        plt.axis('off')
+        plt.subplot(2, 1, 2)
+        plt.title("V2 VReflectivity")
+        plt.imshow(reflectance_img, cmap='gray',
+                vmin=np.min(reflectance_img[reflectance_img > 0]),
+                vmax=np.percentile(reflectance_img, 99))
+        
+        plt.axis('off')
+        plt.tight_layout();   plt.show()
+
+
+        plt.figure(figsize=(12, 12))
+        for i in range(len(cl_maps)):
+            plt.subplot(len(cl_maps), 1, i+1)
+            plt.imshow(cl_maps[i])
+            plt.axis('off')
+            plt.tight_layout()
+        
+        plt.show()
+
+    return (depth_img, reflectance_img, proj_x, proj_y, beeth_points, points_2_image_indices)
+  
+
